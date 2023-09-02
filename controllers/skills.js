3d0c3f424de74350669d254116643cb1a92cecda
Artin-Simonian <artinsimonian@yahoo.com>
2023-09-02 15:24:05 -0700

    lab 2 finalzing

--- controllers/skills.js
+++ controllers/skills.js
@@ -1,21 +1,42 @@
-const skill = require('../models/skills')
-
+const skill = require("../models/skills");
 
 module.exports = {
-    index,
-    show
+  index,
+  show,
+  new: newSkill,
+  create,
+  delete: deleteSkill,
 };
 
-function show(res, res){
+function deleteSkill(req, res) {
+  skill.deleteOne(req.params.id);
+  res.redirect("/skills");
+}
 
-    // Do not use a leading slash when rendering
-    res.render('skills/show', {
-        skill: skill.getOne(req.params.id)
-    })
+function newSkill(req, res) {
+  res.render("skills/new", { title: "New Skills" });
+}
+
+function create(req, res) {
+  console.log(req.body);
+  // The model is responsible for creating data
+  // Todo.create(req.body);
+  // Do a redirect anytime data is changed
+  res.redirect("/skills");
+}
+
+function show(res, res) {
+  // Do not use a leading slash when rendering
+  res.render("skills/show", {
+    skill: skill.getOne(req.params.id),
+    // Don't forget to add the comma above
+    title: "Skills Details",
+  });
 }
 
 function index(req, res) {
-    res.render('skills/index', {
-      skills: skill.getAll()
-    });
-  }
+  res.render("skills/index", {
+    skills: skill.getAll(),
+    title: 'All skills'
+  });
+}
